82f10f996155887b2d1a3288b16f9f0d7ca53ab0
shaikhnehal-1 <shaikhnehal.digital@gmail.com>
2024-09-28 14:58:34 +0530

    updates js

---
+++ Day-1/index.js
@@ -0,0 +1,33 @@
+// 1. Data types
+// String
+// Number
+// Boolean
+// Undefined
+// null
+
+//1.1 String
+// "" '' ``
+//ex : "Vashi" 'awdiz', "1", etc...
+
+// function -> A black that has multiple lines of code that returns various features
+
+console.log("Nehal");
+
+console.log(typeof "Nehal");
+console.log(typeof "Nehal");
+console.log(typeof `Nehal`);
+console.log(typeof 1);
+console.log(typeof 1.5);
+console.log(typeof true);
+console.log(typeof false);
+
+//2. Variable
+// var -> store data, var name = data/info, "=" is used to assign values to the variables
+// let
+// const
+
+var myName = "Nehal";
+console.log(myName, "myName")
+
+var myAge = 20;
+console.log(myAge, "myAge")
