d471d394a22a7006a68db9705f9efadf8791dae5
shaikhnehal-1 <shaikhnehal.digital@gmail.com>
2024-10-19 21:00:31 +0530

    update

---
+++ Day-11/index.js
@@ -0,0 +1,29 @@
+//Binary Search - Day 11
+var array = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+var target = 9;
+
+function BinarySearch() {
+  var left = 0;
+  var right = array.length - 1;
+
+  while (left <= right) {
+    console.log(left, "left", right, "right");
+    const midIndex = Math.floor((left + right) / 2);
+    console.log(midIndex, "midIndex", array[midIndex]);
+
+    if (array[midIndex] == target) {
+      //5 // 2 -> false
+      console.log(array[midIndex], target, "array[midIndex] == target");
+      return midIndex;
+    } else if (array[midIndex] > target) {
+      // 5 > 2 -> true
+      right = midIndex - 1; //4 - 1 -> 3-> 4
+      console.log(array[midIndex], target, "array[midIndex] > target");
+    } else if (array[midIndex] < target) {
+      left = midIndex + 1;
+      console.log(array[midIndex], target, "array[midIndex] < target");
+    }
+  }
+}
+
+console.log(BinarySearch(array, target));
